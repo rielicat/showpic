bac29245f13786e9b12d22d133ae8f080caee920
pyscriptbug <catrielguillen@gmail.com>
2021-06-10 17:41:20 -0400

    photo hover added

--- src/components/user-profile/photos.tsx
+++ src/components/user-profile/photos.tsx
@@ -29,7 +29,7 @@ export default function Photos({ photos }: Props) {
                 style={{ maxHeight: 400 }}
               />
             ))
-          : photos.map(({ docId, photoId, caption }) => (
+          : photos.map(({ docId, photoId, caption, likes, comments }) => (
               <div key={docId} className="relative group">
                 <Img
                   skeletonSize={{ height: "36vw" }}
@@ -37,6 +37,42 @@ export default function Photos({ photos }: Props) {
                   src={images[`${photoId}.jpg`]}
                   alt={caption}
                 />
+                <p className="hidden cursor-pointer absolute bottom-0 left-0 bg-gray-200 z-10 w-full justify-evenly items-center h-full bg-black-faded group-hover:flex">
+                  <p className="flex select-none items-center text-white font-bold">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-8 mr-4"
+                      fill={"none"}
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1}
+                        d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                      />
+                    </svg>
+                    {likes.length}
+                  </p>
+                  <p className="flex select-none items-center text-white font-bold">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-8 mr-4"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1}
+                        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                      />
+                    </svg>
+                    {comments.length}
+                  </p>
+                </p>
               </div>
             ))}
       </div>
